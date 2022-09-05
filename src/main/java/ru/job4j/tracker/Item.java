0495d85462d97ec212447766b303d312d84001a8
irinashakis <shakisi@mail.ru>
2022-09-05 17:11:39 +0300

    7. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,15 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+
+    LocalDateTime created = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+    String createdDateTimeFormat = created.format(formatter);
 
     public Item() {
     }
@@ -30,5 +37,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getCreatedDateTimeFormat() {
+        return createdDateTimeFormat;
     }
 }
