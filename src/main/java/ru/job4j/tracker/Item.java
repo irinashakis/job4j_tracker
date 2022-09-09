b6b863aafe52b07805f5f6a1795836194a7cac11
irinashakis <shakisi@mail.ru>
2022-09-09 18:38:11 +0300

    7. Date. Отображение даты. [#310017 #339852]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,17 +1,12 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
-/*import java.util.Arrays;*/
 
 public class Item {
     private int id;
     private String name;
 
     private LocalDateTime created = LocalDateTime.now();
-
-    /* private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;*/
 
     public Item() {
     }
@@ -24,17 +19,6 @@ public class Item {
         this.id = id;
         this.name = name;
     }
-
-  /*  public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
-        return item;
-    }
-
-    public Item[] findAll() {
-        Item[] newItems = Arrays.copyOf(items, size);
-        return newItems;
-    }*/
 
     public int getId() {
         return id;
