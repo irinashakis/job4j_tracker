12ecba8740c7f4112689ed77488c1a9aba1eec27
irinashakis <shakisi@mail.ru>
2022-09-10 20:50:58 +0300

    6. Метод замены заявки. Tracker.replace [#211748

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -62,6 +62,5 @@ public class Tracker {
         }
         return rsl;
     }
-
 }
 
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -64,5 +64,4 @@ public class TrackerTest {
         tracker.replace(id, bugWithDesc);
         assertThat(tracker.findById(id).getName()).isEqualTo("Bug with description");
     }
-
 }
