f7fe1b915f9ae285101522fae98798a38594d52a
irinashakis <shakisi@mail.ru>
2022-09-10 20:09:04 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -49,16 +49,5 @@ public class Tracker {
         }
         return rsl;
     }
-
-    private int indexOf(int id) {
-        int rsl = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
-                rsl = index;
-                break;
-            }
-        }
-        return rsl;
-    }
 }
 
