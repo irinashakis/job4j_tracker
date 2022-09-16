a4461c92417f1fa6d24fd72e7a9819d7d0fda910
irinashakis <shakisi@mail.ru>
2022-09-16 18:50:47 +0300

    2.8. Реализация класса StartUI. Вывод заявок по имени [#500749 #342167]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -59,15 +59,15 @@ public class StartUI {
                 }
             } else if (select == 5) {
                 System.out.println("=== Find items by name ===");
-                 System.out.print("Enter name: ");
-                 String name = scanner.nextLine();
-                 Item[] items = tracker.findByName(name);
-                 if (items.length > 0) {
-                     for (Item item : items) {
-                     System.out.println(item);
-                     }
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item[] items = tracker.findByName(name);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
                 } else {
-                System.out.println("Заявки с именем: " + name + " не найдены.");
+                    System.out.println("Заявки с именем: " + name + " не найдены.");
                 }
             } else if (select == 6) {
                 run = false;
