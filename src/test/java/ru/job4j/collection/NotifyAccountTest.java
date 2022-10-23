93a43ea4cb287321f98f7938b6976272d2e624fb
irinashakis <shakisi@mail.ru>
2022-10-23 17:25:02 +0300

    3. Модели данных и HashSet. [#218530]

---
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NotifyAccountTest {
+
+    @Test
+    public void whenTwoAccountsAddSent() {
+        NotifyAccount notifyAccount = new NotifyAccount();
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(notifyAccount.sent(accounts)).containsAll(expect);
+    }
+
+    @Test
+    public void whenAllDuplicate() {
+        NotifyAccount notifyAccount = new NotifyAccount();
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(notifyAccount.sent(accounts)).containsAll(expect);
+    }
+
+    @Test
+    public void whenOneDuplicate() {
+        NotifyAccount notifyAccount = new NotifyAccount();
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001"),
+                new Account("345", "Petr Arsentev", "111111")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(notifyAccount.sent(accounts)).containsAll(expect);
+    }
+}
