63166bbb76246bcc070a18f3e6061de0137d9b92
irinashakis <shakisi@mail.ru>
2022-10-23 16:58:38 +0300

    2. Идентичные тексты [#212655]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -19,5 +19,4 @@ public class UniqueText {
         }
         return rsl;
     }
-
 }
--- src/test/java/ru/job4j/collection/UniqueTextTest.java
+++ src/test/java/ru/job4j/collection/UniqueTextTest.java
@@ -3,8 +3,6 @@ package ru.job4j.collection;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-
 class UniqueTextTest {
 
     @Test
