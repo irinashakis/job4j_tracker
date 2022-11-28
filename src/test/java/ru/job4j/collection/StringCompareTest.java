bec77896ae0e6b1ad50d018c7c9a473c07c701b1
irinashakis <shakisi@mail.ru>
2022-11-28 14:26:21 +0300

    3. Компаратор для строк [#35008]

---
+++ src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -0,0 +1,76 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCompareTest {
+    @Test
+    public void whenStringsAreEqualThenZero() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(rst).isEqualTo(0);
+    }
+
+    @Test
+    public void whenLeftLessThanRightResultShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(rst).isLessThan(0);
+    }
+
+    @Test
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Ivanova"
+        );
+        assertThat(rst).isGreaterThan(0);
+    }
+
+    @Test
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(rst).isGreaterThan(0);
+    }
+
+    @Test
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Patrova",
+                "Petrov"
+        );
+        assertThat(rst).isLessThan(0);
+    }
+
+    @Test
+    public void whenLastCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petroz",
+                "Petrov"
+        );
+        assertThat(rst).isGreaterThan(0);
+    }
+
+    @Test
+    public void whenLeftVeryLessThanRightResultShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "I",
+                "Ivanov"
+        );
+        assertThat(rst).isLessThan(0);
+    }
+}
